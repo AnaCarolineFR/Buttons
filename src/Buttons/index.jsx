3d31d0cfd6897fb7a703e689c6ef2bc484d224b5
AnaCarolineFR <carol_ferreira2011@hotmail.com>
2022-03-07 12:29:18 -0300

    Botões plataforma next

---
+++ src/Buttons/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button } from 'antd';
+
+
+export function Buttons(){
+   return(
+  <>
+    <Button type="primary" className="button1">Poppins</Button>
+    <br/>
+    <Button type="primary" className="button2">Poppins</Button>
+    <br />
+    <Button type="primary" className="button3">Poppins</Button>
+    <br />
+    <Button type="primary" className="button4">Poppins</Button>
+    <br />
+    <Button type="primary" className="button5">Poppins</Button>
+    <br />
+    <Button type="primary" className="button6">Poppins</Button>
+    <br />
+    <Button type="primary" className="button7">Poppins</Button>
+    <br />
+    <Button type="primary" className="button8">Poppins</Button>
+    <br />
+    <Button type="primary" className="button9">X</Button>
+    <br />
+    <Button type="primary" className="button10">Poppins</Button>
+    <br />
+    <Button type="primary" className="button11">Poppins</Button>
+    <br />
+
+  </>
+  
+);
+ 
+}
+   
+export default Buttons();
